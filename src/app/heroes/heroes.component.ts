8b9a2e299ce34ea4e6de2438b8c4c06682a461fb
Viki2-0 <v.tischenko.tecza@gmail.com>
2020-11-01 13:00:16 +0100

    added messages

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,12 +11,12 @@ import { HeroService } from '../hero.service';
  export class HeroesComponent implements OnInit {
   hero: Hero = { id: 1, name: 'WindStorm'}; 
   heroes: Hero [];
-  constructor(private heroService: HeroService) { }
-
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
     console.log(this.selectedHero);
   }
 
