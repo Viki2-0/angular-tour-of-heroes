074b806ba3137b40c931be486ab30ab56fbcd9d1
Viki2-0 <v.tischenko.tecza@gmail.com>
2020-11-22 21:35:34 +0100

    DashBoard!!

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,15 @@
 import { Hero } from './hero';
 
-export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
+export const 
+HEROES: Hero[] = [
+  { id: 11, name: 'MicroSoft' },
   { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
+  { id: 13, name: 'Bombasta' },
   { id: 14, name: 'Celeritas' },
   { id: 15, name: 'Magneta' },
   { id: 16, name: 'RubberMan' },
   { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
+  { id: 18, name: 'FireWall' },
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
 ];
