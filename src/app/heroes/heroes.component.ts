7b0bdf7bb7ab2ace6f7e347fbcf9d518b7eeeab8
Viki2-0 <v.tischenko.tecza@gmail.com>
2021-01-20 20:47:54 +0100

    Added "add" hero functionality

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,6 +12,7 @@ import { Location } from '@angular/common';
   styleUrls: ['./heroes.component.css']
 })
  export class HeroesComponent implements OnInit {
+
   hero: Hero = { id: 1, name: 'WindStorm'}; 
   heroes: Hero [];
   constructor(private heroService: HeroService, private messageService: MessageService) { }
@@ -23,5 +24,14 @@ import { Location } from '@angular/common';
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
   }
 }
